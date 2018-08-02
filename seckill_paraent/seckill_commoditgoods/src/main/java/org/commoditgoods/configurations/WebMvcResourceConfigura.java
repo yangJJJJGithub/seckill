34045301299e3e1dc518f0cf2794dc025e97f21e
yangj <445347627@qq.com>
2018-08-02 17:43:32 +0800

    一些配置

---
+++ seckill_paraent/seckill_commoditgoods/src/main/java/org/commoditgoods/configurations/WebMvcResourceConfigura.java
@@ -0,0 +1,21 @@
+package org.commoditgoods.configurations;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan
+public class WebMvcResourceConfigura extends WebMvcConfigurerAdapter{
+	
+	/**
+	 * 静态资源访问
+	 */
+	public void addResourceHandlers(ResourceHandlerRegistry register){
+		register.addResourceHandler("/**").addResourceLocations("classpath:/static/");
+		super.addResourceHandlers(register);
+	}
+}
